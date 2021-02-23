e9292bb956f2726c6151993d07abab293500c68d
Krushang Kasundra <kruzkasu223@gmail.com>
2021-02-23 19:00:59 +0530

    learnt functions typing

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -153,3 +153,34 @@ ninja = { name: 25, age: 'yoshi' };
 console.log(ninja);
 */
 /* ***************************************************** */
+/*
+// Function...
+
+// let greet: Function = () => {
+//   console.log('hello, world');
+// }
+
+// greet = 'hello';
+
+// greet = () => {
+//   console.log('hello, again');
+// }
+
+const add = (a: number, b: number, c?: number | string = 10): void => {
+    console.log(a + b);
+    console.log(c);
+}
+
+add(5, 10, 'ninja');
+
+const minus = (a: number, b: number): number => {
+    return a + b;
+}
+
+let result = minus(10,7);
+console.log(result);
+*/
+/* ***************************************************** */
+/*
+
+*/
