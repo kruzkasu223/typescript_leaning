30cd6d4658e0f8fd22fcefc8ff84fe3ce232e663
Krushang Kasundra <kruzkasu223@gmail.com>
2021-02-23 18:02:58 +0530

    learnt types, arrays and objects...

--- script.js
+++ script.js
@@ -1,6 +1,73 @@
-var character = 'kruz';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-inputs.forEach(function (input) {
-    console.log(input);
-});
+/*
+// You can use JavaScript in TypeScript...
+
+const character = 'kruz'
+console.log(character)
+
+const inputs = document.querySelectorAll('input')
+
+inputs.forEach(input => {
+    console.log(input)
+})
+
+*/
+/* ***************************************************** */
+/*
+// Variable Types in TS
+
+let character = 'kruz'
+let age = 20
+let isTall = false
+
+// character = 20
+character = 'axit'
+
+// age = 'frnd'
+age = 21
+
+// isTall = 50
+isTall = true
+
+let stringhere: string = 'string'
+let numberhere: number = 10
+let booleanhere: boolean = true
+let anythinghere: any = 'askdj' | 392 | false
+
+
+const circ = (diameter: number) => {
+    return diameter * Math.PI
+}
+
+console.log(circ(10))
+
+*/
+/* ***************************************************** */
+// Arrays and Objects
+// arrays
+var names = ['luigi', 'mario', 'yoshi'];
+names.push('toad');
+// names.push(3);
+// names[1] = 3;
+var numbers = [10, 20, 12, 15];
+numbers.push(25);
+// numbers.push('shaun');
+// numbers[0] = 'shaun';
+var mixed = ['ken', 4, 'chun-li', 8, 9];
+mixed.push('ryu');
+mixed.push(10);
+mixed[0] = 3;
+// objects
+var ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
+};
+ninja.age = 40;
+ninja.name = 'ryu';
+// ninja.age = '30';
+// ninja.skills = ['fighting', 'sneaking']
+ninja = {
+    name: 'yoshi',
+    belt: 'orange',
+    age: 40
+};
--- script.ts
+++ script.ts
@@ -1,3 +1,6 @@
+/*
+// You can use JavaScript in TypeScript...
+
 const character = 'kruz'
 console.log(character)
 
@@ -6,3 +9,76 @@ const inputs = document.querySelectorAll('input')
 inputs.forEach(input => {
     console.log(input)
 })
+
+*/
+/* ***************************************************** */
+/*
+// Variable Types in TS
+
+let character = 'kruz'
+let age = 20
+let isTall = false
+
+// character = 20
+character = 'axit'
+
+// age = 'frnd'
+age = 21
+
+// isTall = 50
+isTall = true
+
+let stringhere: string = 'string'
+let numberhere: number = 10
+let booleanhere: boolean = true
+let anythinghere: any = 'askdj' | 392 | false
+
+
+const circ = (diameter: number) => {
+    return diameter * Math.PI
+}
+
+console.log(circ(10))
+
+*/
+/* ***************************************************** */
+
+// Arrays and Objects
+
+// arrays
+let names = ['luigi', 'mario', 'yoshi'];
+
+names.push('toad');
+// names.push(3);
+// names[1] = 3;
+
+let numbers = [10, 20, 12, 15];
+
+numbers.push(25);
+// numbers.push('shaun');
+// numbers[0] = 'shaun';
+
+let mixed = ['ken', 4, 'chun-li', 8, 9];
+
+mixed.push('ryu');
+mixed.push(10);
+mixed[0] = 3;
+
+// objects
+let ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
+};
+
+ninja.age = 40;
+ninja.name = 'ryu';
+// ninja.age = '30';
+// ninja.skills = ['fighting', 'sneaking']
+
+ninja = {
+    name: 'yoshi',
+    belt: 'orange',
+    age: 40,
+    // skills: ['running'],
+};
