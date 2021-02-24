ff70cd93fe2bf86aa57e81e3bea5b7ae3e9da79f
Krushang Kasundra <kruzkasu223@gmail.com>
2021-02-24 14:20:33 +0530

    selected html elements and typecasting...

--- public/script.js
+++ public/script.js
@@ -1,2 +1,10 @@
 "use strict";
-console.log('hi, hello');
+var form = document.querySelector('.new-item-form');
+var type = document.querySelector('#type');
+var tofrom = document.querySelector('#tofrom');
+var details = document.querySelector('#details');
+var amount = document.querySelector('#amount');
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
--- sandbox.ts
+++ sandbox.ts
@@ -231,3 +231,17 @@ logDetails = (ninja: {name: string, age: number}) => {
 }
 */
 /* ***************************************************** */
+/*
+// Selecting HTML Elements...
+
+const anchor = document.querySelector('a')!;
+if(anchor) {
+    console.log(anchor.href);
+}
+console.log(anchor.href);
+
+//const form = document.querySelector('form')!;
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+console.log(form.children);
+*/
+/* ***************************************************** */
