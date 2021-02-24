136564daf8d540bbdf63db0af0a13d16a7008d56
Krushang Kasundra <kruzkasu223@gmail.com>
2021-02-24 15:03:51 +0530

    created class and object and some other...

--- public/script.js
+++ public/script.js
@@ -1,4 +1,38 @@
 "use strict";
+var Invoice = /** @class */ (function () {
+    // client: string
+    // details: string
+    // amount: number
+    // constructor(c: string, d: string, a: number){
+    //     this.client = c
+    //     this.details = d
+    //     this.amount = a
+    // }
+    /* ************ */
+    // readonly client: string
+    // private details: string
+    // public amount: number
+    function Invoice(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+    }
+    Invoice.prototype.format = function () {
+        return this.client + " owes \u20B9" + this.amount + " for " + this.details;
+    };
+    return Invoice;
+}());
+var invOne = new Invoice('mario', 'making website', 1000);
+var invTwo = new Invoice('luigi', 'making app', 1500);
+var invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+invoices.forEach(function (inv) {
+    console.log(inv.client, inv.amount, inv.format());
+});
+// invOne.client = 'kruz'
+// invTwo.amount = 1300
+// console.log(invoices)
 var form = document.querySelector('.new-item-form');
 var type = document.querySelector('#type');
 var tofrom = document.querySelector('#tofrom');
--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,45 @@
+class Invoice{
+    // client: string
+    // details: string
+    // amount: number
+
+    // constructor(c: string, d: string, a: number){
+    //     this.client = c
+    //     this.details = d
+    //     this.amount = a
+    // }
+    /* ************ */
+    // readonly client: string
+    // private details: string
+    // public amount: number
+
+    constructor(
+        readonly client: string,
+        private details: string,
+        public amount: number
+    ){}
+
+    format(){
+        return `${this.client} owes ₹${this.amount} for ${this.details}`
+    }
+}
+
+const invOne = new Invoice('mario', 'making website', 1000)
+const invTwo = new Invoice('luigi', 'making app', 1500)
+
+
+let invoices: Invoice[] = []
+invoices.push(invOne)
+invoices.push(invTwo)
+
+invoices.forEach(inv => {
+    console.log(inv.client, inv.amount, inv.format())
+})
+
+// invOne.client = 'kruz'
+// invTwo.amount = 1300
+// console.log(invoices)
+
 const form = document.querySelector('.new-item-form') as HTMLFormElement
 
 const type = document.querySelector('#type') as HTMLSelectElement
