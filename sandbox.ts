986d1c756dca4b723db456b3a0ca41401ce9c40e
Krushang Kasundra <kruzkasu223@gmail.com>
2021-02-24 16:43:02 +0530

    learnt generics, enums and tupples...

--- sandbox.ts
+++ sandbox.ts
@@ -280,6 +280,94 @@ greetPerson(me);
 */
 /* ***************************************************** */
 /*
+// GENERICS
+
+// const addUID = (obj: object) => {
+//   let uid = Math.floor(Math.random() * 100);
+//   return {...obj, uid};
+// }
+
+// const addUID = <T extends object>(obj: T) => {
+//   let uid = Math.floor(Math.random() * 100);
+//   return {...obj, uid};
+// }
+
+const addUID = <T extends {name: string}>(obj: T) => {
+    let uid = Math.floor(Math.random() * 100);
+    return {...obj, uid};
+}
+
+let docOne = addUID({name: 'yoshi', age: 40});
+//let docTwo = addUID('shaun');
+
+console.log(docOne.name);
+
+// with interfaces
+interface Resource<T> {
+    uid: number;
+    resourceName: string;
+    data: T;
+}
+
+const docThree: Resource<object> = {
+    uid: 1, 
+    resourceName: 'person', 
+    data: { name: 'shaun' }
+};
+
+const docFour: Resource<string[]> = {
+    uid: 1, 
+    resourceName: 'shoppingList', 
+    data: ['bread', 'milk']
+};
+
+console.log(docThree, docFour);
+*/
+/* ***************************************************** */
+/*
+// ENUMS
+
+enum ResourceType { BOOK, AUTHOR, FILM, DIRECTOR };
+
+interface Resource<T> {
+    uid: number;
+    resourceType: ResourceType;
+    data: T;
+}
+
+const docOne: Resource<object> = {
+    uid: 1,
+    resourceType: ResourceType.BOOK,
+    data: { title: 'name of the wind' }
+}
+const docTwo: Resource<object> = {
+    uid: 10,
+    resourceType: ResourceType.DIRECTOR,
+    data: { title: 'name of the wind' }
+}
+
+console.log(docOne);
+console.log(docTwo);
+*/
+/* ***************************************************** */
+/*
+// TUPLES
+
+let arr = ['ryu', 25, true];
+arr[0] = false;
+arr[1] = 'yoshi';
+arr = [30, false, 'yoshi'];
+
+let tup: [string, number, boolean] = ['ryu', 25, true];
+// tup[0] = false;
+tup[0] = 'ken';
+
+let student: [string, number];
+//student = [23564, 'chun-li'];
+student = ['chun-li', 23564];
+*/
+/* ***************************************************** */
+/*
 // Comments from script.ts
 
 
